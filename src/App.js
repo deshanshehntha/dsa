fed6160d70e4c6d76b710f3f019a21e7883da4e8
“w.deshan” <w.deshan@directfn.com>
2019-05-06 17:49:08 +0530

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component} from 'react'
+import {BrowserRouter as Router,Route, Link} from "react-router-dom";
+import logo from "./logo.svg";
+import NavLoginBar from './components/Navbar';
+import Landing from './components/Landing'
+import "bootstrap/dist/css/bootstrap.min.css";
+import SignUpComp from "./components/signup.component";
+import LoginComp from "./components/login.component";
+import Logout from './components/logout.component';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+       <Router>
+         <div className="App">
+             <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                 <a className="navbar-brand" href="http://www.railway.gov.lk/web/" target="_blank" rel="noopener noreferrer">
+                     <img src={logo} width="30" height="30" alt="Sri Lanka Railways"/>
+                 </a>
+                 <Link to="/" className="navbar-brand">Railway Management System</Link>
+                 <div className="collpase navbar-collapse">
+                     <ul className="navbar-nav mr-auto">
+                         <li className="navbar-item">
+                             <Link to="/" className="nav-link">Home</Link>
+                         </li>
+                         <li className="navbar-item">
+                             <Link to="/register" className="nav-link">Register</Link>
+                         </li>
+                         <NavLoginBar isLogged={false}/>
+                     </ul>
+                 </div>
+             </nav>
+             <Route exact path="/" component={Landing}/>
+             <div className="container">
+                 <Route path="/register" component={SignUpComp} />
+                 <Route path="/login" component={LoginComp} />
+                 <Route path="/logout" component={Logout}/>
+
+             </div>
+         </div>
+
+       </Router>
+    );
+  }
 }
-
 export default App;
